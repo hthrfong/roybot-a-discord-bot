12740444e45452a0f0c93f1f6cc60d65e4f13b2f
heather.fong <heather.fong@ligo.org>
2023-01-24 05:06:59 -0800

    Add __init__.py file

---
+++ lib/bot/__init__.py
@@ -0,0 +1,64 @@
+import discord
+import asyncio
+from glob import glob
+from discord.ext.commands import Bot as BotBase
+
+OWNER_IDS = [268862253326008322, 166790627856613376]
+COGS = [path.split("/")[-1][:-3] for path in glob("./lib/cogs/*.py")]
+
+
+class Ready(object):
+    def __init__(self):
+        for cog in COGS:
+            setattr(self, cog, False)
+
+    def ready_up(self, cog):
+        setattr(self, cog, True)
+        print(f" {cog} cog ready")
+
+    def all_ready(self):
+        return all([getattr(self, cog) for cog in COGS])
+
+    
+class Bot(BotBase):
+    def __init__(self):
+        self.command_prefix = "!"
+        
+        self.ready = False
+        self.cogs_read = Ready()
+
+        super().__init__(command_fix=self.command_prefix,
+                         owner_ids=OWNER_IDS,
+                         intents=discord.Intents().all())
+
+    def setup(self):
+        for cogs in COGS:
+            self.load_extension(f"lib.cogs.{cog}")
+            print(f" {cog} cog loaded")
+        print("setup complete")
+
+    def run(self):
+        print("running setup...")
+        self.setup()
+
+        with open("./lib/bot/.env", "r", encoding="utf-8") as tf:
+            keys = tf.readlines()
+        self.TOKEN = keys[0].rstrip('\n')
+        
+        print("running bot...")
+        super().run(self.TOKEN, reconnect=True)
+
+    async def on_ready(self):
+        if not self.ready:
+            while not self.cogs_ready.all_ready():
+                print("not ready")
+                await asyncio.sleep(0.5)
+
+            self.ready = True
+            print("bot logged in as {0.user}".format(self))
+
+        else:
+            print("bot reconnected")
+
+
+bot = Bot()
